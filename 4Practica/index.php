d1b69482defa2a86da30d6fcc0c483c1865b6c63
DominusDrow <galacticovasquez@gmail.com>
2023-02-23 22:21:20 -0600

    login screen modific

--- 4Practica/index.php
+++ 4Practica/index.php
@@ -1,40 +1,45 @@
-<?php	include ('db.php') ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Adopciones</title>
+	<!-- Bootstrap CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
 
-<?php include ('./includes/header.php') ?>
-
-
-<section class="height-80 bg-light">
+<section class="vh-100" style="background-color: #508bfc;">
   <div class="container py-5 h-100">
     <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-        <div class="card bg-dark text-white" style="border-radius: 1rem;">
+        <div class="card shadow-2-strong" style="border-radius: 1rem;">
           <div class="card-body p-5 text-center">
 
-            <div class="mb-md-5 mt-md-4 pb-5">
+            <h3 class="mb-5">Sign in</h3>
 
-              <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
-              <p class="text-white-50 mb-5">Please enter your login and password!</p>
-
-              <div class="form-outline form-white mb-4">
-                <input type="email" id="typeEmailX" class="form-control form-control-lg" />
-                <label class="form-label" for="typeEmailX">Email</label>
-              </div>
-
-              <div class="form-outline form-white mb-4">
-                <input type="password" id="typePasswordX" class="form-control form-control-lg" />
-                <label class="form-label" for="typePasswordX">Password</label>
-              </div>
-
-              <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
-
+            <div class="form-outline mb-4">
+              <input type="email" id="typeEmailX-2" class="form-control form-control-lg" />
+              <label class="form-label" for="typeEmailX-2">Nombre</label>
             </div>
 
+            <div class="form-outline mb-4">
+              <input type="password" id="typePasswordX-2" class="form-control form-control-lg" />
+              <label class="form-label" for="typePasswordX-2">Contraseña</label>
+            </div>
+
+            <button class="btn btn-primary btn-lg btn-block" type="submit">Ingresar</button>
+						
+            <div class="text-center mt-3 small">
+              <p>¿No tienes cuenta? <a href="register.php">Register</a></p>
+            </div>
+            <hr class="my-4">
           </div>
         </div>
       </div>
     </div>
   </div>
-</section>
+</section>  
 
 
 
